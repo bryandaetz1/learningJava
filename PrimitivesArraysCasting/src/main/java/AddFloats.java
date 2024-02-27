52f1aca93d4b2ebad99d10dfda31ff91a03e1d10
bryandaetz1 <bryandaetz@gmail.com>
2024-02-26 18:28:48 -0800

    completed add two floats exercise

---
+++ PrimitivesArraysCasting/src/main/java/AddFloats.java
@@ -0,0 +1,8 @@
+public class AddFloats {
+    public static void main(String[] args) {
+        float x = 4.2f;
+        float y = 8.1f;
+        float sum = x + y;
+        System.out.println("x + y = " + sum);
+    }
+}
