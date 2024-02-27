1b376d20eec924f82625737171025ee0c5612e03
bryandaetz1 <bryandaetz@gmail.com>
2024-02-26 18:25:30 -0800

    completed int values exercise

---
+++ PrimitivesArraysCasting/src/main/java/IntegerValues.java
@@ -0,0 +1,8 @@
+public class IntegerValues {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 9;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
