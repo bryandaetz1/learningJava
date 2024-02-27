0fbe50a9e428bcca781f1eb4d51cdb2c39aade08
bryandaetz1 <bryandaetz@gmail.com>
2024-02-26 18:31:03 -0800

    completed concat strings exercise

---
+++ PrimitivesArraysCasting/src/main/java/ConcatStrings.java
@@ -0,0 +1,8 @@
+public class ConcatStrings {
+    public static void main(String[] args) {
+        String str1 = "basket";
+        String str2 = "ball";
+        String result = str1 + str2;
+        System.out.println(result);
+    }
+}
