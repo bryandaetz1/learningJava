21d5a077fb2efba77d89a555ebcb0cc4341f012d
bryandaetz1 <bryandaetz@gmail.com>
2024-02-27 16:15:16 -0800

    completed string length exercise

---
+++ PrimitivesArraysCasting/src/main/java/StringLength.java
@@ -0,0 +1,7 @@
+public class StringLength {
+    public static void main(String[] args) {
+        String s = "Java Programming";
+        int sLength = s.length();
+        System.out.println(sLength);
+    }
+}
