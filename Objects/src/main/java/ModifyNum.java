74ece2f59497d205bf3ebf89682f7faec1c9bba9
bryandaetz1 <bryandaetz@gmail.com>
2024-03-05 19:48:38 -0800

    completed modify number exercise

---
+++ Objects/src/main/java/ModifyNum.java
@@ -0,0 +1,14 @@
+public class ModifyNum {
+    public static void main(String[] args) {
+        int number = 5;
+        System.out.println("Before: " + number);
+
+        modifyNumber(number);
+        System.out.println("After: " + number);
+
+    }
+
+    public static void modifyNumber(int num) {
+        num = num * 2;
+    }
+}
